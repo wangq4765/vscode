07357f1945d1622e226f2ff1d153e3eb9b8e1660
wangqiannan <3598661291@qq.com>
2024-03-08 23:05:26 +0800

    perform

--- c++/1.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-  int *p;
-  printf("%d",*p);
-  return 0;
-}
-
--- c++/2.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main()
-{
-  int a=5,b=8;
-  float x=4.2,y=3.4,n=float(a+b)/2+(int)x%(int)y;
-  printf("%f\n",n);
-}
